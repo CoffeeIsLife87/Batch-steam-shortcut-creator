1b7888f7a3d582fa25892f1bce0e66432f2ab6ad
superchill93 <36961653+herosilas12@users.noreply.github.com>
2020-03-18 10:46:35 -0500

    removed junk

--- scan.py
+++ scan.py
@@ -5,18 +5,4 @@ for root, dirs, files in os.walk("B:\itch apps"):
         if file.endswith(".exe"):
              result = (os.path.join(root, file))
              print (result)
-
-
-appname = "epic games store"
-exepath = '"C:\Program Files (x86)\Epic Games\Launcher\Portal\Binaries\Win32\EpicGamesLauncher.exe"'
-DirWithExe = '"C:\Program Files (x86)\Epic Games\Launcher\Portal\Binaries\Win32"'
-LuanchOptions = input("do you have launch options, press enter if no")
-if (LuanchOptions == ""):
-    LuanchOptions = ""
-print (LuanchOptions)
     
-    
-
-
-
-print ("AppName ", appname, "Exe" , exepath,"StartDir ", DirWithExe," icon ShortcutPath", LuanchOptions, "ishidden AllowDesktopConfig    AllowOverlay    openvr    Devkit  DevkitGameID  LastPlayTime")

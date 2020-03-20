df22532047fd8d0a621129a324ce41d18fdee36a
superchill93 <36961653+herosilas12@users.noreply.github.com>
2020-03-20 07:08:14 -0500

    added path options

--- maker.py
+++ maker.py
@@ -1,11 +1,12 @@
 result = ("")
+dir1 = input ('what is your itch.io folder (I.E. C:\\Users\\user\\itch\\games)')
+
 import os
-for root, dirs, files in os.walk("B:\itch apps"):
+for root, dirs, files in os.walk(dir1):
     for file in files:
         if file.endswith(".exe"):
              result = (os.path.join(root, file))
-
-
+             print (result)
 
 pathVDF = ("C:\\Program Files (x86)\\Steam\\userdata\\876672695\\config\\shortcuts.vdf ")
 name = ""
@@ -18,14 +19,11 @@ inVRLibrary = ""
 last_playtime = "0 "
 catagories = ""
 
-
-
-
 name = input ("what is the name of the game")
-path = input ("paste the entire path to EXE")
-start = input ("paste directory with exe in it(don't add the exe to the path)")
+path = input ("paste the entire path to EXE(I.E. C:\\Users\\user\\games\\game.exe)")
+start = input ("paste directory with exe in it(I.E. C:\\Users\\user\\games)")
 inVRLibrary = input ("if it is a VR game, press 1, otherwise press 0")
-categories = input ("are there and cagetorgies you want the game in")
+categories = input ("are there and cagetorgies you want the game in(I.E. RTS, FPS, simulation)")
 
 
 print (pathVDF, name, " ",path, " ",start, " ", hidden, allow_desktop_config, allow_steam_overlay, inVRLibrary," ", last_playtime, categories)

0b76395e378b9085aa6f2a40b53bd073494f4171
superchill93 <36961653+herosilas12@users.noreply.github.com>
2020-03-23 12:35:13 -0500

    second try

---
+++ try2.py
@@ -0,0 +1,18 @@
+#second attempt
+import sys, os
+check1 = 0
+itchDIRfile = open("itchDIR.txt", 'r+')
+while (check1 < 1):
+    if itchDIRfile.readline(1) == " ":
+        itchDIR = input("copy and paste itch games directory ")
+        itchDIRfile.write(itchDIR)
+        check1 = 1
+    else:
+        itchDIR = itchDIRfile.readline()
+
+ 
+for root, dirs, files in os.walk(itchDIR):
+    for file in files:
+        if file.endswith(".exe"):
+             exepaths = (os.path.join(root, file))
+              (exepaths, '\n')

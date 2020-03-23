4c56dcfe331834bb1383903fbd827c7ac92c581c
superchill93 <36961653+herosilas12@users.noreply.github.com>
2020-03-23 18:51:31 -0500

    more cleaning

--- maker.py
+++ maker.py
@@ -1,13 +1,27 @@
 import sys, subprocess, os
-result = ("")
-#says where to scan
-dir1 = input ('what is your itch.io folder (I.E. C:\\Users\\user\\itch\\games)')
 #says where the results are written to
-file1 = open('apps.txt','w')
+file1 = open('info\\apps.txt','w')
+#-------------------------------------------------------------------------------
+#functions to define
+def split_path(path):
+  full_path = path
+  path_to_exe, game_name = path.rsplit("\\", 1)
+  return name, full_path, path_to_exe.split(".")[0]
+  path_data = split_path(path)
+#-------------------------------------------------------------------------------
+check1 = 0
+itchDIRfile = open("info\\itchDIR.txt", 'r+')
+while (check1 < 1):
+    if itchDIRfile.readline(1) == " ":
+        itchDIR = input("copy and paste itch games directory ")
+        itchDIRfile.write(itchDIR)
+        check1 = 1
+    else:
+        itchDIR = itchDIRfile.readline()
 #-------------------------------------------------------------------------------
 #scans directory for .exe files
 import os
-for root, dirs, files in os.walk(dir1):
+for root, dirs, files in os.walk(itchDIR):
     for file in files:
         if file.endswith(".exe"):
              result = (os.path.join(root, file))
@@ -17,7 +31,7 @@ for root, dirs, files in os.walk(dir1):
 file1.close()
 #-------------------------------------------------------------------------------
 #checks for steam ID or askes for it if it is not detected
-steamIDfile = open('steamID.txt', 'r+')
+steamIDfile = open('info\\steamID.txt', 'r+')
 if (steamIDfile.readline(1) == ""):
     steamIDpath = "C:\\Program Files (x86)\\Steam\\userdata"
     steamIDpath1 = os.path.realpath(steamIDpath)
@@ -52,25 +66,7 @@ def split_path(path):
   path_data = split_path(path)
 #splits full path into smaller bits 
 #--------------------------------------------------------------------------------
-counter = 0
-amount = 1
-file = open("apps.txt", 'r')
-Efile = open("Exec.txt", 'w+')
-
-while (counter < amount):
-    if (file.readline() == ""):   #checks if the line is empty and moves the script forward if it is
-            counter += 1
-#reads what the line is and splits it
-    else:
-            pathtoedit = file.readline()
-            splitpath = split_path(pathtoedit)
-            print (splitpath)
-            extensions = (pathVDF, splitpath, " ", hidden, allow_desktop_config, allow_steam_overlay, inVRLibrary, last_playtime, categories)
-            supercmd = str((shortcut, extensions))
-            Efile.write(supercmd)
-file.close()
-#reads each line
-#--------------------------------------------------------------------------------
+#extensions = (pathVDF, splitpath, " ", hidden, allow_desktop_config, allow_steam_overlay, inVRLibrary, last_playtime, categories)
 close = input ("press enter to close")
 if (close == ""):
     os.system("python cleanup.py")

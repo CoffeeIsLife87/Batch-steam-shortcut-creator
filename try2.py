3b428e79426fa0526e864d30d7403ac9bea6d7aa
superchill93 <36961653+herosilas12@users.noreply.github.com>
2020-03-23 13:05:44 -0500

    more changes

--- try2.py
+++ try2.py
@@ -1,5 +1,8 @@
 #second attempt
 import sys, os
+pathText = open("apps.txt", 'w+')
+#---------------------------------------------------------------
+#this is the itch.io directory setter
 check1 = 0
 itchDIRfile = open("info\\itchDIR.txt", 'r+')
 while (check1 < 1):
@@ -9,10 +12,23 @@ while (check1 < 1):
         check1 = 1
     else:
         itchDIR = itchDIRfile.readline()
+#---------------------------------------------------------------
+#checks for steam ID and asks for it if not detected 
+steamIDfile = open('info\\steamID.txt', 'r+')
+if (steamIDfile.readline(1) == ""):
+    steamIDpath = "C:\\Program Files (x86)\\Steam\\userdata"
+    steamIDpath1 = os.path.realpath(steamIDpath)
+    os.startfile(steamIDpath)
+    steamID = str(input("copy and paste the numbers from the folder"))
+    steamIDfile.write(steamID)
+    steamIDfile.close()
+else:
+    steamID = steamIDfile.read()
 
- 
+#--------------------------------------------------------------
+
 for root, dirs, files in os.walk(itchDIR):
     for file in files:
         if file.endswith(".exe"):
             exepaths = (os.path.join(root, file))
-            (exepaths, '\n')
+            pathText.write(exepaths, '\n')

0e92c7ee994387cce673251cc3d693d0ced56097
superchill93 <36961653+herosilas12@users.noreply.github.com>
2020-03-24 09:55:09 -0500

    more comments

--- maker.py
+++ maker.py
@@ -44,7 +44,7 @@ allow_steam_overlay = "1 "
 inVRLibrary = "0 "
 last_playtime = "0 "
 categories = ""
-#this is a template
+#this is a template in case I have to move stuff around
 #extensions = (pathVDF+splitresult+" "+hidden+allow_desktop_config+allow_steam_overlay+inVRLibrary+last_playtime+categories)
 #-------------------------------------------------------------------------------
 #scans directory for .exe files

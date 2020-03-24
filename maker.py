912688b00562b6b918070ef82fa0bbd28bae7d32
superchill93 <36961653+herosilas12@users.noreply.github.com>
2020-03-24 07:24:23 -0500

    changes

--- maker.py
+++ maker.py
@@ -5,6 +5,8 @@ def split_path(path):
   full_path = path
   path_to_exe, name = path.rsplit("\\", 1)
   return name, full_path, path_to_exe.split(".")[0]
+
+shortcut = ('python shortcuts.py')
 #-------------------------------------------------------------------------------
 #checking bits
 check1 = 0
@@ -33,19 +35,8 @@ while (check2 < 1):
     else:
         steamID = steamIDfile.read()
         check2 = 1
-#------------------------------------------------------------------------------
 #-------------------------------------------------------------------------------
-#scans directory for .exe files
-import os
-for root, dirs, files in os.walk(itchDIR):
-    for file in files:
-        if file.endswith(".exe"):
-             result = (os.path.join(root, file))
-             splitresult = split_path(result)
-             print (splitresult)
-
-#------------------------------------------------------------------------------
-
+#stuff to define
 pathVDF = ("C:\\Program Files (x86)\\Steam\\userdata\\", steamID, "\\config\\shortcuts.vdf ")
 name = ""
 path = ''
@@ -56,12 +47,16 @@ allow_steam_overlay = "1 "
 inVRLibrary = ""
 last_playtime = "0 "
 categories = ""
+#extensions = (pathVDF, splitpath, " ", hidden, allow_desktop_config, allow_steam_overlay, inVRLibrary, last_playtime, categories)
+#-------------------------------------------------------------------------------
+#scans directory for .exe files
+import os
+for root, dirs, files in os.walk(itchDIR):
+    for file in files:
+        if file.endswith(".exe"):
+             result = (os.path.join(root, file))
+             splitresult = split_path(result)
+             print (splitresult)
 
 #--------------------------------------------------------------------------------
-#steam shortcut maker python script shortcut
-shortcut = ('python shortcuts.py')
-#--------------------------------------------------------------------------------
-#extensions = (pathVDF, splitpath, " ", hidden, allow_desktop_config, allow_steam_overlay, inVRLibrary, last_playtime, categories)
 close = input ("press enter to close")
-if (close == ""):
-    os.system("python cleanup.py")

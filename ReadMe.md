9d677190f4ca1925bb39174c0fa70eaa043bad4d
superchill93 <36961653+herosilas12@users.noreply.github.com>
2020-03-24 11:13:07 -0500

    SO MANY COMMENTS

--- ReadMe.md
+++ ReadMe.md
@@ -2,4 +2,6 @@ in theory it is done but for some reason it doesn't add steam shortcuts
 
 requires python (https://www.python.org/downloads)
 
-https://github.com/CorporalQuesadilla/Steam-Shortcut-Manager (please accept my pull request!!!)
+I used python 3.8.2 but I think and python 3.7.x will work
+
+I use https://github.com/CorporalQuesadilla/Steam-Shortcut-Manager for setting the shortcuts(please accept my pull request!!!)
--- maker.py
+++ maker.py
@@ -1,14 +1,16 @@
-import sys, subprocess, os
+import sys, os
 #-------------------------------------------------------------------------------
 #functions to define
 def split_path(path):
   full_path = path
   start, name = path.rsplit("\\", 1)
+  #that makes sure that everything is spaced properly as well as adds double quotes to the names/paths
   return '"'+name.split(".")[0]+'"'+" "+'"'+path+'"'+" "+'"'+start+'"'
 
+#this will be used later for running the second python script with the arguments after it
 shortcut = ('python shortcuts.py')
 #-------------------------------------------------------------------------------
-#checks for itch.io directory to scan
+#checks for itch.io directory to scan and askes for one if it is not detected
 itchDIRfile = open('info\\itchDIR.txt', 'r+')
 itchDIRcheck = itchDIRfile.readline()
 if (itchDIRcheck == ""):
@@ -24,6 +26,7 @@ steamIDfile = open('info\\steamID.txt', 'r+')
 steamIDcheck = steamIDfile.readline()
 if (steamIDcheck == ""):
     steamIDpath = "C:\\Program Files (x86)\\Steam\\userdata"
+    #opens steam user folder for users so they can copy and paste the numbers that represent their ID
     steamIDpath = os.path.realpath(steamIDpath)
     os.startfile(steamIDpath)
     steamID = str(input("copy and paste the numbers from the folder"))
@@ -33,32 +36,33 @@ else:
     steamID = steamIDcheck
     print (steamID)
 #-------------------------------------------------------------------------------
-#stuff to define
+# A bunch of variables that will be needed later
 pathVDF = ('"'+"C:\\Program Files (x86)\\Steam\\userdata\\"+steamID+"\\config\\shortcuts.vdf"+'"'+" ")
-name = ""
-path = ''
-start = ""
+name = "" #this gets defined later
+path = ''# same
+start = ""# same
 hidden = " 0 "
 allow_desktop_config = "1 "
 allow_steam_overlay = "1 "
-inVRLibrary = "0 "
-last_playtime = "0 "
+inVRLibrary = "0 " # 0 = no, 1 = yes
+last_playtime = "0 " 
 categories = ""
 #this is a template in case I have to move stuff around
 #extensions = (pathVDF+splitresult+" "+hidden+allow_desktop_config+allow_steam_overlay+inVRLibrary+last_playtime+categories)
 #-------------------------------------------------------------------------------
-#scans directory for .exe files
-import os
+#scans set directory for .exe files
 for root, dirs, files in os.walk(itchDIR):
     for file in files:
         if file.endswith(".exe"):
              result = (os.path.join(root, file))
              splitresult = split_path(result)
              #------------------------------------------------------------------
-             #from here @this indentation point, I need to have all the variables set
+             #it is important that it is at this indentation level or it will only set the first one
              extensions = (" "+pathVDF+splitresult+" "+'""'+" "+hidden+allow_desktop_config+allow_steam_overlay+inVRLibrary+last_playtime+categories)
+             #This is when it uses the "shortcut" string thing I set earlier and it uses "extensions as the arguments"
              os.system(shortcut+extensions)
 #--------------------------------------------------------------------------------
+#just a couple of words for the user
 print ("thanks for using my tool")
 print ("glad I could help you automate some stuff")
 close = input ("press enter to close")

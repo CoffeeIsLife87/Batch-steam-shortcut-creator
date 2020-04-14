b34ac0bd0813eff1060dfb32b6dd82be19083d68
superchill93 <36961653+herosilas12@users.noreply.github.com>
2020-04-14 09:38:18 -0500

    started reader.txt and added steam shortcut documentation

---
+++ reader.py
@@ -0,0 +1,4 @@
+# this is my attempt at removing apps that don't exist anymore
+# if I can read the shortcuts.VDF file and determine all of the existing shortcuts than I can scan the itch dir for the exe files
+# I can then delete the bits that contain the data of the unused shortcut
+
